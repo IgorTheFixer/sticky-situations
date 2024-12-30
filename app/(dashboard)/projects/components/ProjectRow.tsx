8bf843fee4c0e28e2422a79b2785cc998d001f77
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2024-12-29 22:26:20 -0500

    added navigation from the projects page

--- app/(dashboard)/projects/components/ProjectRow.tsx
+++ app/(dashboard)/projects/components/ProjectRow.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { useModal } from "@/hooks/useModal";
 import { Project } from "@prisma/client"
+import { useRouter } from "next/navigation";
 
 interface ProjectRowProps {
   initialData: Project
@@ -10,9 +11,11 @@ interface ProjectRowProps {
 
 export function ProjectRow({ initialData }: ProjectRowProps) {
   const modal = useModal();
+  const router = useRouter()
+
   return (
     <tr>
-      <td>{initialData.name}</td>
+      <td onClick={()=>router.push(`/projects/${initialData.id}`)}>{initialData.name}</td>
       <td>{initialData.description}</td>
       <td>
         <Button
