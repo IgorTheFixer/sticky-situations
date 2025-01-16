9deb38a8583b72ce15cce71caccd152a7fdd3c7f
IgorTheFixer <75816411+IgorTheFixer@users.noreply.github.com>
2025-01-16 11:45:00 -0500

    WIP

--- app/api/projects/[projectId]/route.ts
+++ app/api/projects/[projectId]/route.ts
@@ -46,3 +46,40 @@ export async function PATCH(req:Request, { params }: { params: { projectId: stri
     return new NextResponse("Internal error", { status: 500 });
   }
 }
+
+const fetchBooks = async() => {
+  try {
+    const response = await fetch ("/api/v1/books")
+    if (!response.ok){
+      const errorMessage = `${response.status} (${response.statusText})`
+      const error = new Error(errorMessage)
+      throw(error)
+    }
+    const bookData = await response.json()
+    setBooks(bookData.books)
+  } catch (error:any) {
+    console.log(`Error in fetch: ${error.message}`)
+  }
+}
+
+const addBook = async (formPayload) => {
+  try {
+    const response = await fetch('/api/v1/books', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formPayload)
+    })
+    if (!response.ok) {
+      const errorMessage = `${response.status} (${response.statusText})`
+      const error = new Error(errorMessage)
+      throw(error)
+    }
+    const body = await response.json()
+    const currentBooks = books
+    setBooks(currentBooks.concat(body.book))
+  } catch(err) {
+    console.error(`Error in fetch: ${err.message}`)
+  }
+}
